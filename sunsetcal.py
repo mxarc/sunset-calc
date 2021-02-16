5230bd97f9387e9f50a26314963006bf21271d6f
Alfonso Reyes <vaca.hace.muu@gmail.com>
2021-02-15 21:28:05 -0700

    refactor code

--- sunsetcal.py
+++ sunsetcal.py
@@ -1,60 +1,75 @@
 #!/usr/bin/env python3
 import math
+from datetime import date, datetime
 
 # this script calculates the solar position for a given time of the year
 # it can also calculate sunset and sunrise time
 
 # source: https://www.esrl.noaa.gov/gmd/grad/solcalc/solareqns.PDF
 
-dayOfTheYear = 45  # equals february 15
-hour = 20
-mn = 24
-sc = 00
+date_object = datetime.now().timetuple()
+day_of_year = date_object.tm_yday
+days_in_year = 365
+hour = date_object.tm_hour
+mn = date_object.tm_min
+sc = date_object.tm_sec
 latitude = 24.792493
 longitude = -107.4051
 timezone = -7
 
 # first we need to calculate the fractional year, this returns a result in radians
 # for leap years use 366 instead of 365
-fractionalYear = ((2*math.pi)/365)*((dayOfTheYear - 1) + ((hour-12) / 24))
+
+
+def fractionalYear():
+    return (2 * math.pi / days_in_year) * \
+        (day_of_year - 1 + ((hour - 12) / 24))
 
 # from the fractional year we can estimate the equation of time (in minutes)
 # and the solar declination angle (in radians).
 
-eqtime = 229.18 * (0.000075 +
-                   0.001868 * math.cos(fractionalYear) -
-                   0.032077 * math.sin(fractionalYear) -
-                   0.014615 * math.cos(2*fractionalYear) -
-                   0.040849 * math.sin(2*fractionalYear))
 
-declination = math.radians(0.006918 - 0.399912 * math.cos(fractionalYear) +
-                           0.070257 * math.sin(fractionalYear) -
-                           0.006758 * math.cos(2*fractionalYear) +
-                           0.000907 * math.sin(2*fractionalYear) -
-                           0.002697 * math.cos(3*fractionalYear) +
-                           0.00148 * math.sin(3*fractionalYear))
+def eqTime():
+    y = fractionalYear()
+    a = 0.001868 * math.cos(y)
+    b = 0.032077 * math.sin(y)
+    c = 0.014615 * math.cos(2 * y)
+    d = 0.040849 * math.sin(2 * y)
+    return 229.18 * (0.000075 + a - b - c - d)
+
+
+def declinationAngle():
+    y = fractionalYear()
+    a = 0.399912 * math.cos(y)
+    b = 0.070257 * math.sin(y)
+    c = 0.006758 * math.cos(2 * y)
+    d = 0.000907 * math.sin(2 * y)
+    e = 0.002697 * math.cos(3 * y)
+    f = 0.00148 * math.sin(3 * y)
+    return 0.006918 - a + b - c + d - e + f
 
 # now it's time to calculate the true solar time, first we need to find the time offset
-time_offset = eqtime + (4*longitude) - (60*timezone)
 
-tst = hour * 60 + mn + sc/60 + time_offset
+
+def timeOffset():
+    return eqTime() + (4*longitude) - (60*timezone)
+
+
+def trueSolarTime():
+    return hour * 60 + mn + sc/60 + timeOffset()
+
 
 # solar hour angle, in degrees is:
-ha = (tst/4)-180
+def solarHourAngle():
+    return (trueSolarTime()/4)-180
+
 
 # the solar zenith angle can then be found for the hour angle (ha), latitude
 # and solar declination using the following eq
 zenith = math.sin(latitude)*math.sin(declination) + \
     math.cos(latitude) * math.cos(declination) * math.cos(ha)
 
-# and now the solar azimuth, degrees clowise from north is found from
-solar_azimuth = -((math.sin(latitude)*math.cos(zenith)-math.sin(declination)) /
-                  (math.cos(latitude)*math.sin(zenith)))
 
-print(f'fractionalYear: {fractionalYear}')
-print(f'eqtime: {eqtime}')
-print(f'declination: {declination}')
-print(f'time_offset: {time_offset}')
-print(f'tst: {tst}')
-print(f'ha: {ha}')
-print(f'solar_azimuth: {solar_azimuth}')
+print(f'fractionalYear: {fractionalYear()}')
+print(f'eqtime: {eqTime()}')
+print(f'declinationAngle: {declinationAngle()}')
